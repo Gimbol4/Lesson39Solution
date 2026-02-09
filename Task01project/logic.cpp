a86795d7a45c17693ebbc13f241ec725811f1bc8
Gimbol4 <Shishkanov.yd@gmail.com>
2026-02-09 17:07:23 +0300

    Solve 1 task

---
+++ Task01project/logic.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+double over_number(double a) {
+	double num = 1 / a;
+	return num;
+}
--- Task01project/main.cpp
+++ Task01project/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
+double over_number(double a);
+
 int main() {
+	double a;
+	cout << "Input a number: ";
+	cin >> a;
 
-
+	cout << "Your over number is " << over_number(a);
 	return 0;
 }
